8267ca0682b095c16879cb3c39c5ffa3119e4e8a
Carlos Marques <carrlosmr@gmail.com>
2024-10-22 11:15:20 -0300

    feat: adicionando validação se o e-mail já existe e o tamanho minimo da senha ao criar uma conta

--- src/pages/sign_up/SignUpPage.tsx
+++ src/pages/sign_up/SignUpPage.tsx
@@ -11,12 +11,15 @@ import { createUserFirebase } from "../../service/user-service";
 
 export const SignUpPage = () => {
 
-    const {register, formState: {errors}, handleSubmit, watch} = useForm<SignUpForm>();
+    const {register, formState: {errors}, handleSubmit, watch, setError} = useForm<SignUpForm>();
 
     const watchPassword = watch("password");
 
     const handleSubmitPress = async (data: SignUpForm) => {
-        await createUserFirebase(data);
+        const response = await createUserFirebase(data);
+        if(response != null){
+            setError(response.campo, {type: response.type})
+        }
     }
 
     return (
@@ -29,35 +32,60 @@ export const SignUpPage = () => {
 
                     <SignUpInputContainer>
                         <p>Nome</p>
-                        <CustomInput placeholder="Digite seu nome" hasError={!!errors?.name} {...register("name", {required: true})} />
+                        <CustomInput 
+                            placeholder="Digite seu nome" 
+                            hasError={!!errors?.name} 
+                            {...register("name", {required: true})} 
+                        />
                         {errors?.name?.type === "required" && <InputErrorMessage children="O campo nome é obrigatório"/>}
                     </SignUpInputContainer>
 
                     <SignUpInputContainer>
                         <p>Sobrenome</p>
-                        <CustomInput placeholder="Digite seu sobrenome" hasError={!!errors?.lastName} {...register("lastName", {required: true})} />
+                        <CustomInput 
+                            placeholder="Digite seu sobrenome" 
+                            hasError={!!errors?.lastName} 
+                            {...register("lastName", {required: true})} 
+                        />
                         {errors?.lastName?.type === "required" && <InputErrorMessage children="O campo sobrenome é obrigatório" />}
                     </SignUpInputContainer>
 
                     <SignUpInputContainer>
                         <p>E-mail</p>
-                        <CustomInput placeholder="Digite seu e-mail" hasError={!!errors?.email} {...register("email", {required: true, validate: (value) => validator.isEmail(value)})} />
+                        <CustomInput 
+                            placeholder="Digite seu e-mail" 
+                            hasError={!!errors?.email} 
+                            {...register("email", {required: true, validate: (value) => validator.isEmail(value)})} 
+                        />
                         {errors?.email?.type === "required" && <InputErrorMessage children="O campo e-mail é obrigatório" />}
                         {errors?.email?.type === "validate" && <InputErrorMessage children="E-mail inválido" />}
+                        {errors?.email?.type === "alreadyInUse" && <InputErrorMessage children="Este e-mail já está cadastrado" />}
                     </SignUpInputContainer>
 
                     <SignUpInputContainer>
                         <p>Senha</p>
-                        <CustomInput type="password" placeholder="Digite sua senha" hasError={!!errors?.password} {...register("password", {required: true})} />
+                        <CustomInput 
+                            type="password" 
+                            placeholder="Digite sua senha" 
+                            hasError={!!errors?.password} 
+                            {...register("password", {required: true, minLength: 6})} 
+                        />
                         {errors?.password?.type === "required" && <InputErrorMessage children="O campo senha é obrigatório" />}
+                        {errors?.password?.type === "minLength" && <InputErrorMessage children="A senha deve ter pelo menos 6 caracteres" />}
                     </SignUpInputContainer>
 
                     <SignUpInputContainer>
                         <p>Confirmar senha</p>
-                        <CustomInput type="password" placeholder="Digite novamente sua senha" hasError={!!errors?.confirmPassword} 
-                            {...register("confirmPassword", {required: true, validate: (value) => value == watchPassword})} />
+                        <CustomInput 
+                            type="password" 
+                            placeholder="Digite novamente sua senha" 
+                            hasError={!!errors?.confirmPassword} 
+                            {...register("confirmPassword", {required: true, minLength: 6, validate: (value) => value == watchPassword})} 
+                        />
                         {errors?.confirmPassword?.type === "required" && <InputErrorMessage children="O campo confirmar senha é obrigatório" />}
                         {errors?.confirmPassword?.type === "validate" && <InputErrorMessage children="O campo confirmar senha deve ser igual a senha" />}
+                        {errors?.confirmPassword?.type === "minLength" && <InputErrorMessage children="A confirmação de senha deve ter pelo menos 6 caracteres" />}
+
                     </SignUpInputContainer>
 
                     <CustomButton startIcon={<FiLogIn size={18} />} onClick={() => handleSubmit(handleSubmitPress)()}>Criar conta</CustomButton>
--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,9 +1,22 @@
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { AuthError, AuthErrorCodes, createUserWithEmailAndPassword } from "firebase/auth"
 import { auth, db } from "../config/firebase"
 import { SignUpForm } from "../types/SignUpForm"
 import { addDoc, collection } from "firebase/firestore";
 
-export const createUserFirebase = async (data: SignUpForm) => {
+const FormErrorKeys = {
+    email: "email" as keyof SignUpForm,
+    name: "name" as keyof SignUpForm,
+    lastName: "lastName" as keyof SignUpForm,
+    password: "password" as keyof SignUpForm,
+    confirmPassword: "confirmPassword" as keyof SignUpForm,
+};
+
+interface FormError {
+    campo: keyof SignUpForm;
+    type: string;
+}
+
+export const createUserFirebase = async (data: SignUpForm): Promise<null | FormError> => {
     try{
         const userCredentials = await createUserWithEmailAndPassword(auth, data.email, data.password);
 
@@ -13,7 +26,16 @@ export const createUserFirebase = async (data: SignUpForm) => {
             lastName: data.lastName,
             email: userCredentials.user.email
         });
-    }catch (err){
 
+        return null;
+
+    }catch (error){
+        const _error = error as AuthError;
+        
+        if(_error.code === AuthErrorCodes.EMAIL_EXISTS){
+            return {campo: FormErrorKeys.email, type: "alreadyInUse"};
+        }
+
+        return null;
     }
 }
